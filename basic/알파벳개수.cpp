123a7e3c9c7549655463241478114b3ce3e89ac9
solbae <sol4854@naver.com>
2023-02-28 23:02:16 +0900

    알파벳 개수

---
+++ basic/알파벳개수.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int n, ret;
+// vector<int> v;
+
+// main(){
+//     for(int i = 1; i < n; i++){
+//         if(i > 9){
+//             getN(i);
+//         }
+//         if()
+//         v.clear();
+//     }
+// }
+
+// void getN(int i){
+//     while(i){
+//         v.push_back(i % 10);
+//         i /= 10;
+//     }
+// }
