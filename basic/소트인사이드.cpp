d09444ef958da8faebd3b2af97baad4b38bfb9eb
solbae <sol4854@naver.com>
2023-03-01 03:14:59 +0900

    소트인사이트

---
+++ basic/소트인사이드.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string n;
+
+bool compare(char &a, char &b){
+    return a > b;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    sort(n.begin(), n.end(), compare);
+
+    cout << n;
+}
