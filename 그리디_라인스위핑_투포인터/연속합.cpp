d6e35d84aa154c3e5bff2652b9a8beb333fd5831
solbae <sol4854@naver.com>
2023-04-02 23:53:07 +0900

    연속합 수정

--- 그리디_라인스위핑_투포인터/연속합.cpp
+++ 그리디_라인스위핑_투포인터/연속합.cpp
@@ -17,10 +17,11 @@ int main(){
 	for(int i = 0; i < n; i++){
 		cin >> num;
 		sum += num;
-		if(sum < 0) sum = 0;
 		ret = max(ret, sum);
+		if(sum < 0) sum = 0;
 
 	}
+
 	cout << ret << '\n';
 	return 0;
 }
