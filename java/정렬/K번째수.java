54967e7e387c1515d9e8328f4f6164186307d29b
solbae <sol4854@naver.com>
2023-05-01 12:03:49 +0900

    K번째수

--- java/정렬/K번째수.java
+++ java/정렬/K번째수.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class K번째수 {
+package 정렬;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        return answer;
+    }
 }
