9fc7281df27e4e00545c6de660d4f2008b5e8064
solbae <sol4854@naver.com>
2023-05-04 12:06:06 +0900

    점수 집계

--- java/정렬/점수집계.java
+++ java/정렬/점수집계.java
@@ -1,50 +1,40 @@
 package 정렬;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class 시리얼번호2 {
+public class 점수집계 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String[] arr = new String[N];
+        int T = sc.nextInt();
+        int[] arr = new int[5];
 
-        for(int i = 0; i < N; i++){
-            arr[i] = sc.next();
-        }
-
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() < o2.length()) {
-                    if(add(o1) == add(o2)){
-                        return o1.compareTo(o2);
-                    }else {
-                        return Integer.compare(add(o1), add(o2));
-                    }
-                } else {
-                    return 1;
-                }
+        while(T-- > 0){
+            for(int i = 0; i < 5; i++){
+                arr[i] = sc.nextInt();
             }
-        });
+            Arrays.sort(arr);
 
-        for(String i : arr){
-            System.out.println(i);
-        }
-    }
-    static int add(String str){
-        int sum = 0;
-        for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-            if(ch >= '0' && ch <= '9'){
-                sum += ch - '0';
+            arr[0] = 0;
+            arr[4] = 0;
+//            System.out.printf(Arrays.toString(arr));
+
+            int sum = 0;
+            int mx = -1;
+            int min = 11;
+            for(int i = 1; i <= 3; i++){
+                sum += arr[i];
+                mx = Math.max(mx, arr[i]);
+                min = Math.min(min, arr[i]);
+            }
+
+            if(Math.abs(mx - min) >= 4){
+                System.out.println("KIN");
+            }else{
+                System.out.println(sum);
             }
         }
-        return sum;
     }
-
 }
