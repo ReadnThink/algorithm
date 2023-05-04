18ac7a3d8fc859023767381c2efac1378798c849
solbae <sol4854@naver.com>
2023-05-04 12:05:47 +0900

    소트 인사이드

--- java/정렬/소트인사이드.java
+++ java/정렬/소트인사이드.java
@@ -1,39 +1,25 @@
 package 정렬;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
-public class 점수집계 {
+
+public class 소트인사이드 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int[] arr = new int[5];
+        int n = sc.nextInt();
 
-        while(T-- > 0){
-            for(int i = 0; i < 5; i++){
-                arr[i] = sc.nextInt();
-            }
-            Arrays.sort(arr);
+        int[] counting = new int[10];
 
-            arr[0] = 0;
-            arr[4] = 0;
-//            System.out.printf(Arrays.toString(arr));
+        while(n > 0){
+            counting[n % 10]++;
+            n /= 10;
+        }
 
-            int sum = 0;
-            int mx = -1;
-            int min = 11;
-            for(int i = 1; i <= 3; i++){
-                sum += arr[i];
-                mx = Math.max(mx, arr[i]);
-                min = Math.min(min, arr[i]);
-            }
-
-            if(Math.abs(mx - min) >= 4){
-                System.out.println("KIN");
-            }else{
-                System.out.println(sum);
+        for(int i = 9; i >= 0; i--){
+            while(counting[i] > 0){
+                System.out.print(i);
+                counting[i]--;
             }
         }
     }
