1e3a16f8cdace83fd578aa48d298e7f89e82ec20
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-07-17 12:09:49 +0900

    보석쇼핑

--- python/프로그래머스/카카오/LV_3/보석쇼핑.py
+++ python/프로그래머스/카카오/LV_3/보석쇼핑.py
@@ -1,29 +1,31 @@
-def search(dic, length, gems):
-    start, end = 0, len(gems)-1
-    
-    while True:
-        print(f'{dic[gems[end]] = }')
-        dic[gems[end]] = dic.get(gems[end], 0) - 1
-        if dic[gems[end]] == 0:
-            break
-        end -= 1
-    while True:
-        print(f'{dic[gems[start]] = }')
-        dic[gems[start]] = dic.get(gems[start], 0) - 1
-        if dic[gems[start]] == 0:
-            break
-        start += 1
-        
-    return start+1, end+1
-
 def solution(gems):
-    dic = dict()
-    for gem in gems:
-        dic[gem] = dic.get(gem, 0) + 1
+    N, diamond_size = len(gems), len(set(gems))
+    candi = []
+    start, end = 0,0
+    dic = {gems[0]:1}
+    while end != N or start != N:
+        if len(dic) == diamond_size:
+            candi.append([start+1,end+1, end-start])
+            dic[gems[start]] -= 1
+            if dic[gems[start]] == 0:
+                del dic[gems[start]]
+            start += 1
+        else:
+            end += 1
+            if end == N:
+                break
+            dic[gems[end]] = dic.get(gems[end],0) + 1
+            
     
-    length = len(dic)
-    # print(f'{dic = }, {length = }')
+        print(dic)
+    
+        
+    candi.sort(key=lambda x : x[2])
+    print('result!',candi)
+    answer = [candi[0][0],candi[0][1]]
+    return answer
 
-    i,j = search(dic,length,gems)
-    return [i,j]
-
+# print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+# print(solution(["AA", "AB", "AC", "AA", "AC"]))
+# print(solution(["XYZ", "XYZ", "XYZ"]))
+print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"]))
