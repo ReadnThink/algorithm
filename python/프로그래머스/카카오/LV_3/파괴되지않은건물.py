daed6aa17079f2c1249f3457f764b2254ca59181
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-01 10:57:45 +0900

    파괴되지 않은 건물 효율성 통과

--- python/프로그래머스/카카오/LV_3/파괴되지않은건물.py
+++ python/프로그래머스/카카오/LV_3/파괴되지않은건물.py
@@ -1,27 +1,37 @@
-def go(board,s):
-    t,sy, sx, ey, ex, degree = s[0],s[1],s[2],s[3],s[4], s[5]
-    
-    for i in range(sy,ey+1):
-        for j in range(sx,ex+1):
-            if t == 1:
-                board[i][j] -= degree
-            elif t == 2:
-                board[i][j] += degree
-    # for i in range(len(board)):
-    #     print(board[i])
-    # print()
-    return board
-
-def count(board):
-    result = 0
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] > 0:
-                result += 1
-    return result
+ATTACK = 1
+RECOVER = 2
 
 def solution(board, skill):
-    for s in skill:
-        board = go(board,s)        
-    answer = count(board)
+    update(board, skill)
+    
+    answer = 0
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if is_not_destroyed(board,r,c):
+                answer += 1
     return answer
+
+def is_not_destroyed(board, r, c):
+    return board[r][c] > 0
+
+def update(board, skill):
+    diff = [[0]*(len(board[0]) + 1) for _ in range(len(board)+1)]
+    for type, r1, c1, r2, c2, degree in skill:
+        d = -degree if type == ATTACK else degree
+        diff[r1][c1] += d
+        diff[r1][c2+1] += -d
+        diff[r2+1][c1] += -d
+        diff[r2+1][c2+1] += d
+        
+    for r in range(len(board)):
+        for c in range(1, len(board[0])):
+            diff[r][c] += diff[r][c-1]
+                
+    for c in range(len(board[0])):
+        for r in range(1, len(board)):
+            diff[r][c] += diff[r-1][c]
+            
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            board[r][c] += diff[r][c]
+    
