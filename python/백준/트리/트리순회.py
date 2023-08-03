fabc86149fddd6188eb5d9b9dfbeaa6cd9cacce7
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-03 11:50:59 +0900

    트리순회

--- python/백준/트리/트리순회.py
+++ python/백준/트리/트리순회.py
@@ -1,31 +1,35 @@
 import sys
 input = sys.stdin.readline
-
-def preorder(node):
-    print(node,end='')
-    for val in tree[node]:
-        if not val == '.':
-            preorder(val)
-            
-def inorder(node):
-    if node != '.':
-        inorder(tree[node][0])
-        print(node,end='')
-        inorder(tree[node][1])
-                
-def postorder(node):    
-    if node != '.':
-        postorder(tree[node][0])
-        postorder(tree[node][1])
-        print(node,end='')
-        
+sys.setrecursionlimit(1000000)
 
 N = int(input())
-visited = [False]*N
 tree = {}
-for i in range(N):
-    root, left, right = input().split()
-    tree[root] = [left, right]
+
+for _ in range(N):
+    root, left, right = input().strip().split()
+    tree[root] = [left,right]
+
+def preorder(node):
+    if node == '.':
+        return
+    print(node,end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+
+def inorder(node):
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node,end='')
+    inorder(tree[node][1])
+
+def postorder(node):
+    if node == '.':
+        return
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node,end='')
+    pass
 
 preorder('A')
 print()
