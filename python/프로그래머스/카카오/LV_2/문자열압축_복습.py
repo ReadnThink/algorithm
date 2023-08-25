d7ff367c535bae0a24d60a1aa5117e46de3aaf01
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-25 11:49:29 +0900

    문자열압축

--- python/프로그래머스/카카오/LV_2/문자열압축_복습.py
+++ python/프로그래머스/카카오/LV_2/문자열압축_복습.py
@@ -1,21 +1,22 @@
 def solution(s):
-    length = len(s)
-    answer = length
-    for interval in range(1,(length // 2)+1):
-        zip_s = 0
+    n = len(s)
+    answer = n
+    
+    for interval in range(1, n//2+1):
+        zip_s = ''
         start = 0
-        while start + interval <= length:
+        while start+interval <= n:
             repeat = 1
-            sub = s[start: start + interval]
+            sub = s[start: start+interval]
             while sub == s[start+(interval*repeat): start+(interval*(repeat+1))]:
                 repeat += 1
             if repeat > 1:
-                zip_s += len(str(repeat))
-            zip_s += len(sub)
+                zip_s += str(repeat)
+            zip_s += sub
             start += interval * repeat
-            
-        zip_s += len(s[start:])
-        # print(f"{zip_s=}, {interval=}")
-        answer = min(answer, zip_s)
-    
+        
+        zip_s += s[start:]
+        print(f'{interval = }, {zip_s = }')
+        
+        answer = min(answer, len(zip_s))    
     return answer
