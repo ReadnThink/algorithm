369f8d43d96fc94652082a0397bbdf78e947d5be
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-10-12 12:06:25 +0900

    1로만들기 DP loop 수정

--- python/백준/DP/1로만들기/1로만들기_DP_loop.py
+++ python/백준/DP/1로만들기/1로만들기_DP_loop.py
@@ -13,9 +13,9 @@ dp[1] = 0
 for i in range(2, n+1):
     dp[i] = dp[i-1]+1
     if i % 3 == 0:
-        dp[i] = min(dp[i//3], dp[i-1])+1
+        dp[i] = min(dp[i//3]+1, dp[i])
     if i % 2 == 0:
-        dp[i] = min(dp[i//2], dp[i-1])+1
+        dp[i] = min(dp[i//2]+1, dp[i])
     
 
 print(dp[n])
