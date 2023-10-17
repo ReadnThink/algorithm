ec2b0ccea5ad9f25e5900993963a7e5441ed43a6
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-10-17 10:56:50 +0900

    회의실 배정

--- python/백준/그리디_라인스위핑_투포인터/1931_회의실배정.py
+++ python/백준/그리디_라인스위핑_투포인터/1931_회의실배정.py
@@ -2,18 +2,15 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-str = input().strip()
-boom = input().strip()
-stk = []
-boom_len = len(boom)
-for s in str:
-    stk.append(s)
-    if len(stk) >= boom_len:
-        if ''.join(stk[len(stk)-boom_len:]) == boom:
-            for i in boom:
-                stk.pop()
+n = int(input().strip())
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr.sort(key=lambda x : (x[1],x[0]))
     
-if len(stk) == 0:
-    print('FRULA')
-else:
-    print(''.join(stk))
+end_time = 0
+ret = 0
+for start, end in arr:
+    if end_time <= start:
+        end_time = end
+        ret += 1
+
+print(ret)
