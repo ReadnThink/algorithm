513fdf5364e03b0d8a3c052a5e06003a8b2648b3
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-10-17 11:14:15 +0900

    소가 강을 건넌 이유3

--- python/백준/그리디_라인스위핑_투포인터/14469_소가길을건너간이유3.py
+++ python/백준/그리디_라인스위핑_투포인터/14469_소가길을건너간이유3.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-cow = list(list(map(int, input().split()) )for _ in range(N))
-cow.sort(key=lambda x : (x[0], x[1]))
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
 
-time = 0
+arr.sort(key=lambda x : x[0])
 
-for arrive, scan_time in cow:
-    if arrive > time:
-        time = arrive
-    time += scan_time
+ret = 0
 
-print(time)
+for arrive, time in arr:
+    if ret < arrive:
+        ret = arrive
+    ret += time
+
+print(ret)
