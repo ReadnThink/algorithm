8e4246dab8ada6c754cdde04834f80f5fc13a4f0
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-10-23 11:53:51 +0900

    점프

---
+++ python/백준/DP/점프.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
+dp[0][0] = 1
+for i in range(n):
+    for j in range(n):
+        jump = arr[i][j]
+        if  dp[i][j] > 0 and arr[i][j] > 0:
+            if j+jump < n:
+                dp[i][j+jump] = dp[i][j+jump] + dp[i][j]
+            if i+jump < n:
+                dp[i+jump][j] = dp[i+jump][j] + dp[i][j]
+
+print(dp[n-1][n-1])
