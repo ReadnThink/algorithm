21f3fb6e4a5fd62042c4efa0e4e6bbb3cb395962
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-10-25 12:12:54 +0900

    기타레이

--- python/백준/이분탐색/2792_보석상자_복습.py
+++ python/백준/이분탐색/2792_보석상자_복습.py
@@ -1,6 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-N,M = list(map(int, input().split()))
-arr = [int(input()) for _ in range(M)]
+def check(mid):
+    if mx > mid:
+        return False
+    cnt = 0
+    sum = 0
+    
+    for v in arr:
+        if sum + v <= mid:
+            sum += v
+        else:
+            sum = v
+            cnt += 1
+    if sum:
+        cnt += 1
+    # print(mid, ' cnt = ', cnt,' sum = ', sum)
+    return cnt <= M
 
+N,M = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+left,right = 1, sys.maxsize
+ret,mx = sys.maxsize,max(arr)
+
+while left <= right:
+    mid = (left+right) // 2
+    
+    if check(mid):
+        ret = min(ret, mid)
+        right = mid - 1
+        
+    else:
+        left = mid + 1
+print(ret)
