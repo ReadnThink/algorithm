b2bf805f37ce155997410eeddd61b4fbfdc27a34
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-11-23 11:28:51 +0900

    뮤탈리스크

--- python/백준/완탐_백트래킹/뮤탈리스크.py
+++ python/백준/완탐_백트래킹/뮤탈리스크.py
@@ -1,43 +1,45 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
 from collections import deque
 
-PER = []
-def per(depth, visited, arr):
-    if depth == n:
-        PER.append(arr[:])
+def per(depth, temp):
+    if depth == 3:
+        candidates.append(temp[:])
         return
     for i in range(3):
         if visited[i]:
             continue
         visited[i] = True
-        arr.append(attacks[i])
-        per(depth+1,visited,arr)
+        temp.append(ATTACK[i])
+        per(depth+1, temp)
+        temp.pop()
         visited[i] = False
-        arr.pop()
 
-ret = 1e9       
-def bfs():
-    global ret
+def bfs(scvs):
     q = deque()
-    q.append([scv[0],scv[1],scv[2], 1])
+    q.append([scvs[0], scvs[1], scvs[2], 1])
     
     while q:
         a,b,c,cnt = q.popleft()
         
-        for attack in PER:
-            na = a - attack[0]
-            nb = b - attack[1]
-            nc = c - attack[2]
+        for attack in candidates:
+            na = max(0, a-attack[0])
+            nb = max(0, b-attack[1])
+            nc = max(0, c-attack[2])
             
-            if na <= 0 and nb <= 0 and nc <= 0:
-                ret = min(ret, cnt)
-                continue
+            if na == 0 and nb == 0 and nc == 0:
+                return cnt
             q.append([na,nb,nc,cnt+1])
+    
+ATTACK = [9,3,1]
+N = int(input())
+SCVS = list(map(int, input().split()))
+while len(SCVS) < 3:
+    SCVS.append(0)
+ret = 0
+visited = [False]*3
+candidates = []
 
-n = int(input())
-scv = list(map(int, input().split()))
-attacks = [9,3,1]
-per(0,[False]*n,[])
-bfs()
-print(ret)
+per(0,[])
+print(bfs(SCVS))
